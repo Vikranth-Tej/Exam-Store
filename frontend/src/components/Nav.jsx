57830136ae0a8a54846adb31a508de05f24fc737
JayanthC <jayanth.c0407@gmail.com>
2025-02-17 16:18:39 +0530

    nav bar intial commit

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import image from "../assets/react.svg"
+import { IoSearchSharp } from "react-icons/io5";
 
 const Nav = () => {
   return (
-    <div className="w-full h-full absolute bg-gradient-to-r from-blue-600 to-white-2">
-      <header className="flex justify-between bg-white py-6 px-8 md:px-32 drop-shadow-md ">
-        <a href="#">
-          <img src={image} alt="" />
-        </a>
-      </header>
-    </div>
+    <nav className="w-full h-20 flex items-center justify-between px-5 fixed top-0 z-20 bg-[#459ad2]">
+      <h1 className="text-3xl bg-gradient-to-r from-black to-white bg-clip-text text-transparent font-bold">Exam-Store</h1>
+
+      <form className="hidden sm:flex w-1/3 h-10 bg-white items-center justify-center rounded-2xl overflow-hidden">
+        <input type="text" placeholder="Search Papers..." className="w-[70%] h-full px-5 text-lg border-none outline-none" />
+        <button className="w-[30%] h-full flex items-center justify-end pr-5 bg-white border-none">
+          <IoSearchSharp />
+        </button>
+      </form>
+
+      <ul className="hidden sm:flex items-center justify-center gap-5 text-black list-none text-lg pr-2">
+        <li className="cursor-pointer hover:text-cyan-300 hover:border-b-2 border-white">
+          Home
+        </li>
+        <li className="cursor-pointer hover:text-cyan-300 hover:border-b-2 border-white">
+          About
+        </li>
+        <li className="cursor-pointer hover:text-cyan-300 hover:border-b-2 border-white">
+          Contact
+        </li>
+      </ul>
+
+       
+      
+    </nav>
   );
 };
 
